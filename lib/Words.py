7a1db38a4db9f6b3f75ee6d37f962a31f3ed3a21
Max <80323354+GuyBritish@users.noreply.github.com>
2021-05-21 23:27:22 +0700

    Add word-list and hangman states

---
+++ lib/Words.py
@@ -0,0 +1,23 @@
+# This is the words database for the Simple_Hangman game (currently have 20 words)
+wordsDB = [
+    "rebellion",
+    "alcohol",
+    "dividend",
+    "situation",
+    "bolt",
+    "throw",
+    "socialist",
+    "scholar",
+    "hypnotize",
+    "chimney",
+    "fantasy",
+    "unpleasant",
+    "withdraw",
+    "army",
+    "elbow",
+    "disaster",
+    "imperial",
+    "coincide",
+    "mourning",
+    "ideology"
+]
